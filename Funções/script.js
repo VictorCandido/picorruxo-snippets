4c93f247435574f554ad0720d5a3eda3c0f18aae
Victor Candido <victor.candido@iv2.com.br>
2019-11-01 16:06:28 -0300

    Adicionado função de detectar sistema mobile

--- Funções/script.js
+++ Funções/script.js
@@ -21,3 +21,21 @@ function somenteNumeros(num) {
         campo.value = "";
     }
 }
+
+/**
+ * Função que retorna se o codigo está sendo executando em um sistema mobile ou não.
+ */
+function detectar_mobile() {
+    if (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i) ||
+        navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
