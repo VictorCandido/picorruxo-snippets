ffe766757c37efd28575f07d18ad756aefc2012b
vicecsilva@gmail.com <victor.candido@nb-vcandido.grupoiv2.local>
2020-05-18 15:25:30 -0300

    Adicionado Função GetAPI, que faz o consumo de uma API

--- API/script.js
+++ API/script.js
@@ -26,6 +26,27 @@ const getDataset = (dataset, constraint = []) => new Promise((resolve, reject) =
 });
 
 /**
+ * Função que faz uma consulta à uma API e retorna uma promisse.
+ * @param {String} url Url para requisição
+ * @param {String} method Tipo de requisição (GET, PUT, DELETE, POST)
+ * @param {Object Array} body Array de objetos com os dados caso a requisição não seja GET
+ */
+const getAPI = (url, method = 'GET', body = null) => new Promise((resolve, reject) => {
+    fetch(url, {
+        method,
+        redirect: 'follow',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        }),
+        body: body ? JSON.stringify(body) : null
+    }).then(response => method == "DELETE" ? null : response.json()).then((data) => {
+        resolve(data);
+    }).catch((error) => {
+        reject(error);
+    });
+});
+
+/**
  * Função que altera a descrição do processo
  * @param {string} description Valor do dataset a ser consultado
  * @param {int} id id do documento que sera atualizado
